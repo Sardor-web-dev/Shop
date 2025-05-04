3065e3eb66242f1bdf215a143bbb8ef4d98d80d2
SarikViper <dzamolovsardor5@gmail.com>
2025-05-04 22:27:20 +0500

    -

--- components/custom/LoginInBtns.tsx
+++ components/custom/LoginInBtns.tsx
@@ -1,24 +1,38 @@
 "use client";
-import { signIn } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
 const LoginBtns = () => {
   const router = useRouter();
+  const session = useSession();
+  // Fix the bug with btns
   return (
     <>
       <div className="flex gap-2">
-        <button
-          onClick={() => signIn()}
-          className="w-25 h-10 bg-gray-500 p-1 rounded-lg cursor-pointer hover:bg-gray-700 transition-all"
-        >
-          Sign In
-        </button>
-        <button
-          onClick={() => router.push("/admin")}
-          className="w-25 h-10 bg-gray-500 p-1 rounded-lg cursor-pointer hover:bg-gray-700 transition-all"
-        >
-          Go to Admin
-        </button>
+        {session && (
+          <>
+            <button
+              onClick={() => signOut()}
+              className="w-25 h-10 bg-gray-500 p-1 rounded-lg cursor-pointer hover:bg-gray-700 transition-all"
+            >
+              Sign Out
+            </button>
+            <button
+              onClick={() => router.push("/admin")}
+              className="w-25 h-10 bg-gray-500 p-1 rounded-lg cursor-pointer hover:bg-gray-700 transition-all"
+            >
+              Go to Admin
+            </button>
+          </>
+        )}
+        {!session && (
+          <button
+            onClick={() => signIn()}
+            className="w-25 h-10 bg-gray-500 p-1 rounded-lg cursor-pointer hover:bg-gray-700 transition-all"
+          >
+            Sign In
+          </button>
+        )}
       </div>
     </>
   );
